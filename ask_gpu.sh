597026e8421af4dc75e7a53fcf5126ada3849f0d
wadmes <werry715@gmail.com>
2024-10-19 21:37:00 -0400

    update script

--- ask_gpu.sh
+++ ask_gpu.sh
@@ -1,3 +1,3 @@
 module load cudnn8.9-cuda12.3  cuda12.3/toolkit cuda12.3/fft cuda12.3/blas
 conda activate molca
-srun --pty --gres gpu:H100:1 /bin/bash
+srun --time 3-23 --pty --gres gpu:H100:1 /bin/bash
